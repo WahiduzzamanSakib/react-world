a4027ca4cedf5b6af003c533ac626045305f9621
WahiduzzamanSakib <wahidzamanpg@gmail.com>
2026-03-27 15:16:11 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Countries from "./components/Countries/countries"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+<Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Countries/countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const countries = () => {
+    return (
+        <div>
+            <h2>hello</h2>
+        </div>
+    );
+};
+
+export default countries;
